ad473a424ae118446af6688feaf63f017fac8f08
Hai <haingdc@gmail.com>
2020-10-25 19:15:22 +0700

    1 loop with reduce

--- day3-yahtzee-dice/index.test.ts
+++ day3-yahtzee-dice/index.test.ts
@@ -7,9 +7,13 @@ Deno.test('yahtzee', () => {
   var x3 = yahtzee([1, 1, 1, 3, 3]);
   var x4 = yahtzee([1, 2, 3, 4, 5]);
   var x5 = yahtzee([6, 6, 6, 6, 6]);
+  var x6 = yahtzee([1654, 1654, 50995, 30864, 1654, 50995, 22747,
+    1654, 1654, 1654, 1654, 1654, 30864, 4868, 1654, 4868, 1654,
+    30864, 4868, 30864]);
   assertEquals(x1, 10);
   assertEquals(x2, 4);
   assertEquals(x3, 6);
   assertEquals(x4, 5);
   assertEquals(x5, 30);
+  assertEquals(x6, 123456);
 });
--- day3-yahtzee-dice/yahtzee.ts
+++ day3-yahtzee-dice/yahtzee.ts
@@ -1,21 +1,17 @@
 import * as R from 'https://x.nest.land/ramda@0.27.0/source/index.js';
 
-export function yahtzee(numbers: number[]) {
+export function yahtzee(numbers: number[]): number {
   var groupNumber: { [key: string]: number } = {};
+  var initialValue = { max: { dice: 0, sum: 0 }, group: groupNumber }
 
-  numbers.reduce(function sumIntoGroup(group, n) {
+  var result = numbers.reduce(function sumAndMax({ max, group }, n) {
     group[n] = group[n] ? group[n]+n : n;
-    return group;
-  }, groupNumber);
-  // => groupNumber = { "2": 2, "3": 3, "5": 10, "6": 6 }
+    let updateMax = max;
+    if (group[n] > max.sum) {
+      updateMax = { dice: n, sum: group[n] };
+    }
+    return { max: updateMax, group };
+  }, initialValue);
 
-  var groupNumberTuples = Object.keys(groupNumber).map(n => [n, groupNumber[n]]);
-  // groupNumberTuples = [ [ "2", 2 ], [ "3", 3 ], [ "5", 10 ], [ "6", 6 ] ]
-
-  var largest = groupNumberTuples.reduce(function max([numA, sumA], [numB, sumB]) {
-    return sumA > sumB ? [numA, sumA] : [numB, sumB];
-  });
-  // largest = ["5", 10]
-
-  return largest[1];
+  return result.max.sum;
 }
