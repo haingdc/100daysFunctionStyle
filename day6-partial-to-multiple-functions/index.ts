1066ec564ba08027ba65bd156e1e324c638266e6
Hai <haingdc@gmail.com>
2020-10-31 22:53:47 +0700

    partial right

--- day6-partial-to-multiple-functions/index.ts
+++ day6-partial-to-multiple-functions/index.ts
@@ -12,6 +12,10 @@ function quad(a: any, b: any, c: any, d: any) {
   return [a,b,c,d];
 }
 
+function penta(a: any, b: any, c: any, d: any, e: any) {
+  return [a,b,c,d, e];
+}
+
 // prefix first value 'Sean Connery'
 var partial007 = R.pipe( R.partialRight, R.unary )( R.partial, [[ 'Sean Connery' ]] );
 
@@ -33,7 +37,7 @@ console.log(trio007('_', '_'));
 console.log(quad007('_', '_', '_'));
 
 
-
+// Partial multi values
 var [dualFilms, trioFilms, quadFilms] = R.map(
   R.pipe( R.partialRight, R.unary )( R.partial, [[ 'Dr.No', 'From Russia with Love' ]] )
 )
@@ -43,5 +47,16 @@ console.log( dualFilms() );
 console.log( trioFilms('Goldfinger') );
 console.log( quadFilms('Goldfinger', 'Thunderball') );
 
+// Partial right
+// order arguments: later1, later2,..., laterN, prefix1, prefix2, undefined, undefined
+// here undefined for missing arguments
 
+var partialLast007SeanFilm = R.pipe( R.partialRight, R.unary )( R.partialRight, [['Never Say Never Again']] );
+
+// this case, if no value is passed, it will call prefix value first (edge case)
+console.log( partialLast007SeanFilm(dual )(undefined)  );  // => undefined, 'Never Say Never Again'
+
+console.log( partialLast007SeanFilm(trio )('_', '_' )  );  // => '_', '_', 'Never Say Never Again'
+console.log( partialLast007SeanFilm(quad )('_', '_' )  );  // => '_', '_', 'Never Say Never Again', undefined
+console.log( partialLast007SeanFilm(penta)('_', '_')   );  // => '_', '_', 'Never Say Never Again', undefined, undefined
 
