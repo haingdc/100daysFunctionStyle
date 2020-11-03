3bbfaecc82b2b9f9b9fcd5b7fc5e827b172ac21e
Hai <haingdc@gmail.com>
2020-11-03 23:29:13 +0700

    smorse exercise

--- day8/index.ts
+++ day8/index.ts
@@ -1,2 +1,18 @@
 var AtoZ = '.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..'.split(' ');
-console.log(AtoZ);
+var initialValue = { characters: {} as { [key: string]: string}, number: 97 };
+var { characters: smorseDictionary } = AtoZ.reduce(function map({ characters, number }, code) {
+  var char = String.fromCharCode(number);
+  return {
+    characters: { ...characters, [char]: code },
+    number: number + 1,
+  };
+}, initialValue);
+
+function smorse(str: string) {
+  var smorseCodes = str.split('').map(function mapCharacterToSmorseCode(c) {
+    return smorseDictionary[c];
+  });
+  return smorseCodes.join('');
+}
+console.log(smorseDictionary);
+console.log(smorse('sos'));
