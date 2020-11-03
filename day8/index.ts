31d02a7b4d9b37cdb0af346f3a30e2e5e54c0c67
Hai <haingdc@gmail.com>
2020-11-03 23:47:59 +0700

    use Ramda methods

--- day8/index.ts
+++ day8/index.ts
@@ -1,18 +1,36 @@
-var AtoZ = '.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..'.split(' ');
-var initialValue = { characters: {} as { [key: string]: string}, number: 97 };
-var { characters: smorseDictionary } = AtoZ.reduce(function map({ characters, number }, code) {
-  var char = String.fromCharCode(number);
-  return {
-    characters: { ...characters, [char]: code },
-    number: number + 1,
-  };
-}, initialValue);
+import * as R from 'https://x.nest.land/ramda@0.27.0/source/index.js';
 
-function smorse(str: string) {
-  var smorseCodes = str.split('').map(function mapCharacterToSmorseCode(c) {
-    return smorseDictionary[c];
-  });
-  return smorseCodes.join('');
+function getSmorseDictionary() {
+  var AtoZ = '.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..'.split(' ');
+  var initialValue = { characters: {} as { [key: string]: string}, number: 97 };
+  var { characters: smorseDictionary } = AtoZ.reduce(function map({ characters, number }, code) {
+    var char = String.fromCharCode(number);
+    return {
+      characters: { ...characters, [char]: code },
+      number: number + 1,
+    };
+  }, initialValue);
+  return smorseDictionary;
+}
+
+var smorseDictionary = getSmorseDictionary();
+var split = R.invoker(1, 'split');
+
+function smorse(str: string, dictionary: { [key: string]: string }) {
+  // var smorseCodes = str.split('').map(function mapCharacterToSmorseCode(c) {
+  //   return smorseDictionary[c];
+  // });
+  // return smorseCodes.join('');
+
+  function toSmorseCode(c) {
+    return dictionary[c];
+  }
+
+  return R.pipe(
+    split(''),
+    R.map(toSmorseCode),
+    R.join('')
+  )(str);
 }
 console.log(smorseDictionary);
-console.log(smorse('sos'));
+console.log(smorse('sos', smorseDictionary));
