2e64ea041ab20be4abe6fb0d75696a6ff2e0c479
Hai <haingdc@gmail.com>
2020-11-11 03:58:38 +0700

    add source

--- day8-smorse/index.ts
+++ day8-smorse/index.ts
@@ -51,3 +51,4 @@ var countCodes = R.reduce(countCode, initialValue);
 
 console.log( countCodes(smorseCodes) )
 
+// https://www.reddit.com/r/dailyprogrammer/comments/cmd1hb/20190805_challenge_380_easy_smooshed_morse_code_1/
