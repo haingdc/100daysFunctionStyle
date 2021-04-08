eca1a95caae783b816efc52795a3c0414461fdcc
Hai <haingdc@gmail.com>
2021-04-08 21:31:00 +0700

    exercise 2

--- node/day9/exercise.js
+++ node/day9/exercise.js
@@ -9,3 +9,15 @@ var sum = xs => List(xs).map(Sum).fold(Sum.empty())
 QUnit.test("Ex1: sum", assert => {
   assert.equal(String(sum([1,2,3])), "Sum(6)")
 })
+
+// Ex2: reimplement lessThanZero using foldMap and the Any Monoid
+// =========================
+
+var anyLessThanZero = xs =>
+  List(xs).map(x => Any(x < 0)).fold(Any.empty())
+
+
+QUnit.test("Ex2: anyLessThanZero", assert => {
+  assert.equal(String(anyLessThanZero([-2, 0, 4])), "Any(true)")
+   assert.equal(String(anyLessThanZero([2, 0, 4])), "Any(false)")
+})
