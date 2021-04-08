3c55cc39ee7dfe44fa1d594f01d856f56933a182
Hai <haingdc@gmail.com>
2021-04-08 21:10:29 +0700

    exercise to fold

--- node/day9/exercise.js
+++ node/day9/exercise.js
@@ -1,5 +1,9 @@
+const { List } = require('immutable-ext')
+
 // const foldMap = (t, empty, xs) => xs.map(t).reduce((acc, x) => acc.concat(x), empty)
 var sum = xs => List(xs).foldMap(Sum, Sum.empty())
+
+var sum = xs => List(xs).map(Sum).fold(Sum.empty())
 
 
 QUnit.test("Ex1: sum", assert => {
