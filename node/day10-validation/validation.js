d713bad2c772f8a27dc7a9b3339aaffa7c69cd0a
Hai <haingdc@gmail.com>
2021-04-10 13:41:33 +0700

    add demo

--- node/day10-validation/validation.js
+++ node/day10-validation/validation.js
@@ -1,3 +1,37 @@
-const validations = { name: isPresent, email: isEmail.concat(isPresent) }
+const { List } = require('immutable-ext')
+const { Either } = require('./types')
+const { Right, Left } = Either;
 
-isPresent(obj.name).concat( isEmail( obj.email ) )
+const isPresent = x => !!x
+
+const Success = x =>
+({
+  isFail: false,
+  x,
+  fold: (f, g) => g(x),
+  concat: other =>
+    other.isFail ? other : Success(x)
+})
+
+const Fail = x =>
+({
+  isFail: true,
+  fold: (f, g) => f(x),
+  x,
+  concat: other =>
+    other.isFail ? Fail(x.concat(other.x)) : Fail(x)
+})
+
+const validate = (spec, obj) =>
+  List(Object.keys(spec)).foldMap(key =>
+    spec[key]( obj[key] ) ?
+                            Success( [obj] )
+                          :
+                            Fail( [`${key} bad`] )
+  , Success( [obj] ))
+
+const validations = { name: isPresent, email: isPresent }
+const obj = { name: 'brian', email: 'brain@brian.com' }
+
+const res = validate(validations, obj)
+res.fold(console.log, console.log)
