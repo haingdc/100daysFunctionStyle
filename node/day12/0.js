3b63a9310ff770490320cb942ab18b4a0ed2e54b
Hai <haingdc@gmail.com>
2021-04-23 22:41:52 +0700

    get value of Task will be fork

--- node/day12/0.js
+++ node/day12/0.js
@@ -11,7 +11,7 @@ Sum.empty = () => Sum(0)
 
 
 var res = Id.of(  Right(Task.of(Sum(2)))  ) .concat( Id.of(  Right(Task.of(Sum(3)))  ) )
-res.extract().fold(console.error, console.log)
+res.extract().fold(console.error, v => v.fork(console.error, console.log))
 
 
 
