f9e5c885de075c639780f598aacce2b84d2f8b69
Hai <haingdc@gmail.com>
2021-04-23 14:20:47 +0700

    add demo

---
+++ node/day12/0.js
@@ -0,0 +1,17 @@
+var { Either, Task, Id } = require('../lib/types')
+var { Right, Left } = Either
+
+var Sum = x =>
+({
+  x,
+  concat: other =>
+    Sum(x + other.x)
+})
+Sum.empty = () => Sum(0)
+
+
+var res = Id.of(  Right(Task.of(Sum(2)))  ) .concat( Id.of(  Right(Task.of(Sum(3)))  ) )
+res.extract().fold(console.error, console.log)
+
+
+
